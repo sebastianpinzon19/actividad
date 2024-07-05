e6070b24529bd10995fdc72fe955133482ae5203
05RyderK <166447483+05RyderK@users.noreply.github.com>
2024-07-05 09:41:04 -0500

    Se publica ejercicio 6 beisbol

---
+++ 6. Ejercicio Beisbol/Equipo de Beisbol/BLL/Validaciones.cs
@@ -0,0 +1,22 @@
+﻿namespace BLL
+{
+    public class Validaciones
+    {
+        public static void ValidarNumero(int numero)
+        {
+            if (numero < 0)
+            {
+                throw new ArgumentException("no puede ingresar valores menores a 0");
+            }
+        }
+
+        public static void ValidarString(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentException("El campo no puede estar vacio");
+            }
+        }
+
+    }
+}
