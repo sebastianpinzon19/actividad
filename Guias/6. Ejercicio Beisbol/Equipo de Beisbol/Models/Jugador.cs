23f8eb4e16960ca77de2c298572c834a54d40877
05RyderK <166447483+05RyderK@users.noreply.github.com>
2024-07-05 09:15:13 -0500

    Se publica el ejercicio de beisbol

---
+++ Guias/6. Ejercicio Beisbol/Equipo de Beisbol/Models/Jugador.cs
@@ -0,0 +1,36 @@
+﻿using BLL;
+
+namespace Models
+{
+    public abstract class Jugador
+    {
+        private int numeroUniforme;
+        public int NumeroUniforme
+        {
+            get { return numeroUniforme; }
+            set { Validaciones.ValidarNumero(value); numeroUniforme = value; }
+        }
+
+        private string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { Validaciones.ValidarString(value); nombre = value; }
+        }
+
+        private string posicion;
+        public string Posicion
+        {
+            get { return posicion; }
+            set { Validaciones.ValidarString(value);posicion = value; }
+        }
+
+        public Jugador(int numeroUniforme, string nombre, string posicion)
+        {
+            NumeroUniforme = numeroUniforme;
+            Nombre = nombre;
+            Posicion = posicion;
+        }
+        public abstract string MostrarDatos();
+    }
+}
