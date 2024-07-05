fe701dc3b0a9e5fa54b40361df610c851c64b1e4
05RyderK <166447483+05RyderK@users.noreply.github.com>
2024-07-05 18:55:34 -0500

    se publica el ejercicio 8 divisas

---
+++ 8. Ejercicio Divisas/Divisas/BLL/Validaciones.cs
@@ -0,0 +1,17 @@
+﻿namespace BLL
+{
+    public class Validaciones
+    {
+        public static void ValidarPesos(double pesos)
+        {
+            if (pesos <= 0)
+                throw new ArgumentException("La cantidad a convertir no puede ser igual o menor a cero");
+        }
+
+        public static void ValidarCambio(double cambio)
+        {
+            if (cambio <= 0)
+                throw new ArgumentException("El tipo de cambio no puede ser menor o igual a cero");
+        }
+    }
+}
